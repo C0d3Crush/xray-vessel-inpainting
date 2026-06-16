63fe475ac64c5fe1c54ffd45467bdc838ce56d60
C0d3Crush <lukas.dzielski@gmail.com>
2026-06-16 09:38:51 +0200

    fix: add gradient clipping to prevent exploding gradients

--- src/train.py
+++ src/train.py
@@ -975,6 +975,7 @@ def train_model(
             output = model(img, mask)
             loss, _ = criterion(output, img, mask)
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
 
             train_loss += loss.item()
